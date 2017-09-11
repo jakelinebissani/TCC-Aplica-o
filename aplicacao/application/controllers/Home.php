01281805be8521b89805b87c00eb734387be660c
jakelinebissani <jakeline.bissani@gmail.com>
2017-09-11 01:28:46 -0300

    limpando...

--- aplicacao/application/controllers/Home.php
+++ aplicacao/application/controllers/Home.php
@@ -3,34 +3,11 @@ class Home extends CI_Controller {
 
     public function index()
     {
-        $this->output->enable_profiler(TRUE);
-        $this->load->model("produtos_model");
-        $produtos = $this->produtos_model->buscaTodos();
-
-        $dados = array("produtos" => $produtos);
-        $this->load->helper(array("currency"));
-        $this->load->view("produtos/index.php", $dados);
-    }
-
-    public function formulario() {
-        $this->load->view("produtos/formulario");
+        $this->load->view("home/home.php");
     }
 
     public function novoUsuario() {
         $this->load->view("usuarios/novo");
     }
 
-    public function novo() {
-        $usuarioLogado = $this->session->userdata("usuario_logado");
-        $produto = array(
-            "nome" => $this->input->post("nome"),
-            "descricao" => $this->input->post("descricao"),
-            "preco" => $this->input->post("preco"),
-            "usuario_id" => $usuarioLogado["id"]
-        );
-        $this->load->model("produtos_model");
-        $this->produtos_model->salva($produto);
-        $this->session->set_flashdata("success", "Produto salvo com sucesso");
-        redirect("/");
-    }
 }
--- aplicacao/application/views/home/home.php
+++ aplicacao/application/views/home/home.php
@@ -1,9 +1,57 @@
-<!DOCTYPE html>
 <html>
 <head>
-    <title>Home</title>
+    <link rel="stylesheet" href="<?= base_url("css/bootstrap.css") ?>">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 </head>
 <body>
-<h1>HELLOOOOOOO</h1>
+<div class="container">
+
+    <?php if($this->session->flashdata("success")) : ?>
+        <p class="alert alert-success"><?= $this->session->flashdata("success") ?></p>
+    <?php endif ?>
+
+    <?php if($this->session->flashdata("danger")) : ?>
+        <p class="alert alert-danger"><?= $this->session->flashdata("danger") ?></p>
+    <?php endif ?>
+
+    <?php if($this->session->userdata("usuario_logado")) : ?>
+
+        <?= anchor('usuarios/logout','Logout', array("class" => "btn btn-primary"))?>
+
+    <?php else : ?>
+
+        <h1>Login</h1>
+        <?php
+        echo form_open("usuarios/autenticar");
+
+        echo form_label("Email", "email");
+        echo form_input(array(
+            "name" => "email",
+            "id" => "email",
+            "class" => "form-control",
+            "maxlength" => "255"
+        ));
+
+        echo form_label("Senha", "senha");
+        echo form_password(array(
+            "name" => "senha",
+            "id" => "senha",
+            "class" => "form-control",
+            "maxlength" => "255"
+        ));
+
+        echo form_button(array(
+            "class" => "btn btn-primary",
+            "content" => "Login",
+            "type" => "submit"
+        ));
+
+        echo form_close();
+        ?>
+        <?= anchor('home/novoUsuario','Cadastre-se', array("class" => "btn btn-primary"))?>
+
+    <?php endif ?>
+
+</div>
 </body>
 </html>
